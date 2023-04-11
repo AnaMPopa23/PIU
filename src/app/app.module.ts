38629825aba9e62e6cf602489a6272b8062842c0
AnaMPopa23 <popa.anamaria71@yahoo.com>
2023-04-11 15:51:10 +0300

    online laboratory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+//import { HomeComponent } from './body/home/home.component';
+//import { InventoryComponent } from './body/inventory/inventory.component';
+import {MatButtonModule} from '@angular/material/button';
+//import { FormComponent } from './body/form/form.component';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    InventoryComponent,
+    FormComponent
   ],
   imports: [
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/inventory/inventory.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { FormComponent } from '../form/form.component';
+import { ItemService } from 'src/app/services/item.service';
+import { Item } from 'src/models/item';
+
+@Component({
+  selector: 'app-inventory',
+  templateUrl: './inventory.component.html',
+  styleUrls: ['./inventory.component.scss']
+})
+export class InventoryComponent implements OnInit{
+  error?: string;
+  itemList!: Item[];
+  items : string[] = [
+   "test",
+   "test",
+   "test",
+   "test",
+   "test",
+   "test",
+   "test",
+   "test",
+   "test",
+   "test",
+   "test",
+   "test",
+   "test",
+   "test",
+   "test",
+   "test",
+   "test",
+   "test",
+   
+  ]
+
+  constructor(public dialog: MatDialog, public itemService: ItemService) {}
+
+  getItems(): void {
+    this.itemService.getItems().subscribe((list:Item[]) => {
+      this.itemList = list;
+    }, (err) => {
+        this.error = err.message;
+    })
+  }
+
+  async openDialog() {
+    const dialogRef = this.dialog.open(FormComponent, {
+      width: '250px',
+      data: {item : this.items}
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      console.log("New Item added!")
+    });
+    
+  }
+  ngOnInit(): void {}
+}
